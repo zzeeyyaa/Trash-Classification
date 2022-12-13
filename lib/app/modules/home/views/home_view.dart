0450690ba06487f701270018bb9fe276a8900835
fauziahmilda <fauziah.milda@gmail.com>
2022-12-14 00:37:28 +0700

    add bottom bar to home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,7 +10,7 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0xffF0EAD2),
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
@@ -19,6 +20,95 @@ class HomeView extends GetView<HomeController> {
           'HomeView is working',
           style: TextStyle(fontSize: 20),
         ),
+      ),
+      bottomNavigationBar: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            // color: Colors.amber,
+            width: Get.width,
+            height: Get.height * 0.13,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: Get.width,
+                  height: Get.height * 0.09,
+                  color: Color(0xff6C584C),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        // color: Colors.red,
+                        width: 40,
+                        height: 40,
+                        child: IconButton(
+                            onPressed: () => Get.toNamed(Routes.HOME),
+                            icon: Image.asset(
+                              "assets/buttons/home.png",
+                              fit: BoxFit.fill,
+                              height: 45,
+                            )),
+                      ),
+                      SizedBox(
+                        // color: Colors.green,
+                        width: 40,
+                        height: 40,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Image.asset(
+                              "assets/buttons/info.png",
+                              fit: BoxFit.fill,
+                              height: 45,
+                            )),
+                      ),
+                      SizedBox(
+                        width: 50,
+                        height: 50,
+                      ),
+                      SizedBox(
+                        // color: Colors.amber,
+                        width: 40,
+                        height: 40,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Image.asset(
+                              "assets/buttons/cart.png",
+                              fit: BoxFit.fill,
+                              height: 45,
+                            )),
+                      ),
+                      SizedBox(
+                        // color: Colors.pinkAccent,
+                        width: 40,
+                        height: 40,
+                        child: IconButton(
+                            onPressed: () => Get.toNamed(Routes.PROFILE),
+                            icon: Image.asset(
+                              "assets/buttons/profile.png",
+                              fit: BoxFit.fill,
+                              height: 45,
+                            )),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 80,
+            width: 80,
+            // color: Colors.red,
+            child: IconButton(
+                onPressed: () => Get.toNamed(Routes.CAMERA_DETECTOR),
+                icon: Image.asset(
+                  "assets/buttons/menu-camera.png",
+                  fit: BoxFit.fill,
+                  height: 80,
+                )),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
             return GetMaterialApp(
               debugShowCheckedModeBanner: false,
               title: "Application",
-              initialRoute: Routes.LOGIN,
+              initialRoute: Routes.HOME,
               getPages: AppPages.routes,
             );
           } else {
