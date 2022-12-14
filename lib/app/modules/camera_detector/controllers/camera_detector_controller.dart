08cb0b96d4be1e599af45c5dac2314ee5c050a11
fauziahmilda <fauziah.milda@gmail.com>
2022-12-15 00:49:57 +0700

    add camera lib

--- lib/app/modules/camera_detector/controllers/camera_detector_controller.dart
+++ lib/app/modules/camera_detector/controllers/camera_detector_controller.dart
@@ -1,7 +1,32 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 class CameraDetectorController extends GetxController {
-  //TODO: Implement CameraDetectorController
+  var selectedImagePath = ''.obs;
+  var selectedImageSize = ''.obs;
+
+  void getImage(ImageSource imageSource) async {
+    final pickedFile = await ImagePicker().getImage(source: imageSource);
+    if (pickedFile != null) {
+      //store the path of selected image path
+      selectedImagePath.value = pickedFile.path;
+      selectedImageSize.value =
+          ((File(selectedImagePath.value)).lengthSync() / 1024 / 1024)
+                  .toStringAsFixed(2) +
+              " Mb";
+    } else {
+      //when user doesnt select any image
+      Get.snackbar('Error', 'No image selected',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+    }
+  }
 
   final count = 0.obs;
   @override
