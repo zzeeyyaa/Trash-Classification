0ef0ab34dbe330f50a430701183a656e13210be1
fauziahmilda <fauziah.milda@gmail.com>
2022-12-14 14:34:46 +0700

    update info view

--- lib/app/modules/info/views/info_view.dart
+++ lib/app/modules/info/views/info_view.dart
@@ -11,11 +11,162 @@ class InfoView extends GetView<InfoController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffF0EAD2),
-      body: Center(
-        child: Text(
-          'InfoView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 30, right: 30, top: 10),
+            child: Container(
+              height: 60,
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: Color(0xffCC7A7B),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      child: TextButton(
+                        onPressed: () => Get.back(),
+                        child: Image.asset(
+                          "assets/buttons/previous.png",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 150,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Article",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xffF2E8CF),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+            child: Container(
+              height: Get.height * 0.68,
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: Color(0xffDAD5BF),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Color(0xff849554),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Container(
+                              height: 100,
+                              width: 180,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Judul Artikel Mengenai Sampah",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xffF2E8CF),
+                                    ),
+                                  ),
+                                  Text(
+                                    "Senin, 01 Desember 2022",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Color(0xffF2E8CF),
+                                    ),
+                                  ),
+                                  Text(
+                                    "10:00 WIB",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Color(0xffF2E8CF),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Color(0xffF2E8CF),
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 40,
+                            height: 120,
+                            // color: Color.fromARGB(255, 97, 97, 97),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                  child: TextButton(
+                                      onPressed: () {},
+                                      child: Image.asset(
+                                        "assets/buttons/n.png",
+                                        fit: BoxFit.fill,
+                                      )),
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: Stack(
         alignment: Alignment.topCenter,
